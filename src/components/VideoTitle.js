114abc6111ef452bcfe92f7b0c717382b667e525
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2023-12-19 12:22:30 +0530

    test2

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='w-screen aspect-video pt-[20%] px-12 absolute text-black '>
+    <div className='w-screen aspect-video pt-[20%] px-12 absolute text-white '>
       <h1 className='text-5xl font-bold'>{title}</h1>
       <p className='py-6 text-md w-1/4'>{overview}</p>
       <div>
